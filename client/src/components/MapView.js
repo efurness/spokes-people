845bc3727e81d49a47faf280782e668f68e0db12
Ellen Furness <elasfern@gmail.com>
2024-04-01 16:13:38 -0600

    spacing

--- client/src/components/MapView.js
+++ client/src/components/MapView.js
@@ -70,7 +70,7 @@ const MapView = ({ positions }) => {
       className="container is-centered"
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors<li>Bike back <img src="./greenCircle.png"> | Bike out <img src="./redCircle.png"></li>
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors<li>Bike back <img src="./greenCircle.png">&nbsp;&nbsp; | &nbsp; Bike out &nbsp; <img src="./redCircle.png">&nbsp;</li>
        <ul> <li><strong>6 +<img src="./bikeGreenSm.png"> up to 5<img src="./bikePurpleSm.png"> 0 bikes<img src="./bikeRedSm.png"></strong</li></ul>'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
